39e0ef13146cb7ba44ad8f5f9f457cbbb11d5608
Daniel Aragão <danielfilhoce@gmail.com>
2018-05-23 00:43:22 -0300

    packager

--- infra/services/Selections.js
+++ infra/services/Selections.js
@@ -12,11 +12,11 @@ let Roulette = function(population){
         total += 1/subject.fitness;
     });
 
-    proportion = 1/total;
+    proportion = 100/total;
 
     population.subjects.forEach(subject => {
         subjects_percents.push({
-            percent: proportion * 1/subject.fitness,
+            percent: proportion * (1/subject.fitness),
             subject: subject
         });
     });
@@ -36,13 +36,14 @@ let Roulette = function(population){
 }
 
 function pick_up(subjects_percents){
-    let cursor = Math.random();
+    let cursor = Math.random()*100;
     let accum = 0;
     let bigger_subject = null;
 
     for(let i = 0; i < subjects_percents.length; i++){
         let item = subjects_percents[i];
         bigger_subject = item.subject;
+
         accum += item.percent;
 
         if(accum > cursor){
